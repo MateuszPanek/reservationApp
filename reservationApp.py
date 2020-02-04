49d5f7dfa81c6b7a19a0d18b7a380dc010b7fe03
Mateusz Panek <mateusz.m.panek@gmail.com>
2020-02-04 19:51:31 +0100

    Reservation app : minor modifications - variable name changes

--- reservationApp.py
+++ reservationApp.py
@@ -1,5 +1,5 @@
 from reservationAppConfig import *
-from reservationAppModels import TrainSchema, Train, Ticket, TicketSchema
+from reservationAppModels import Train, Ticket, TicketSchema
 from reservationControllers import buy_ticket
 from flask import request, redirect
 
@@ -22,17 +22,12 @@ def buy(train_id):
     if "number_of_tickets" not in request.json:
         return {"message": f"Missing number of tickets."}, 400
 
-    # Odwołujemy się do funkcji buy_ticket z reservationControllers, przekazując jej wartość pod kluczem
-    # number_of tickets (wydobywając ją z przetworzonego w json obiektu response)
     buy_ticket_info = buy_ticket(train_id, request.json["number_of_tickets"])
 
-    # Przekierowujemy klienta na adres podany w zwrotce przez bank (otrzymany w reservationControllers.buy_ticket)
     return redirect(buy_ticket_info)
 
 @app.route('/paid/<int:ticket_id>', methods=["POST"])
 def mark_as_paid(ticket_id):
-    # Tutaj trafi zwrotka z banku z potwierdzeniem opłacenia biletu. Funkcja mark_as_paid zrobi update biletu w bazie dancyh
-    # i przestawi wartość paid jako True
     status = Ticket.query.filter_by(id=ticket_id).first()
     status.paid = True
     app.logger.info(f'payment_id:{status.id}  paid :{status.paid}')
--- reservationControllers.py
+++ reservationControllers.py
@@ -8,23 +8,15 @@ def buy_ticket(train_id, number_of_tickets):
     if train is None:
         raise ValueError(f"Train {train_id} not found.")
 
-    # W tym momencie w bazie danych PKP tworzony jest bilet ze statusem płatnośći = nieopłacony (False)
     ticket = Ticket(train_id=train_id, paid=False, seats=number_of_tickets)
     db.session.add(ticket)
     db.session.commit()
 
     amount = number_of_tickets * train.price
-    # Request body - przygotowuje słownik który zostanie przekazany do banku i zapisany w jego bazie danych
-    # Callbacki zostaną potem zwrócone - paid_callback trafi do pkp, z informacją o opłaceniu biletu
-    # Client callback będzie użyty do przekierowania klienta na stronę do pobrania biletu
     request_body = {'amount': amount,
                     'client_callback': f'http://127.0.0.1:5001/ticket/{ticket.id}',
                     'paid_callback': f'http://127.0.0.1:5001/paid/{ticket.id}'}
 
-    # request.post wysyła pod podany url request z metodą post - w którym przekazuje słownik powyżej w formacie json
-    # Podstawienie requesta pod zmienną służy zapisaniu pod nią odpowiedz którą zwróci bank (Patrz bankApp endopoint /pay)
-    a = requests.post(url='http://127.0.0.1:5002/pay', json=request_body)
-    # Zwrócona od banku odpowiedź (obiekt Response) zostaje przekształcona w jsona i za pomocą klucza odpakowujemy z niej link
-    # Na który należy przekierować klienta (w celu dokonania płatności) - PATRZ reservationAPP/buy/<int:trainid>
-    link = a.json()['redirect_to']
+    payment_start = requests.post(url='http://127.0.0.1:5002/pay', json=request_body)
+    link = payment_start.json()['redirect_to']
     return link
